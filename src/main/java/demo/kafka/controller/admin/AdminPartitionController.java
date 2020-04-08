c5e05057516f825f60e2dd838cd634fbbbd76646
Your Name <you@example.com>
2020-04-08 20:02:00 +0800

    初步完成admin

--- src/main/java/demo/kafka/controller/admin/AdminPartitionController.java
+++ src/main/java/demo/kafka/controller/admin/AdminPartitionController.java
@@ -1,15 +1,10 @@
 package demo.kafka.controller.admin;
 
 
-import com.alibaba.fastjson.JSONObject;
-import com.google.gson.Gson;
-import demo.kafka.controller.admin.util.AdminClusterUtil;
 import demo.kafka.controller.admin.util.AdminPartitionsUtil;
-import demo.kafka.controller.response.DescribeClusterResultResponse;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.admin.DescribeClusterResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,8 +17,8 @@ public class AdminPartitionController {
 
 
     @ApiOperation(value = "增加topic的分区数量")
-    @GetMapping(value = "/getCluster")
-    public boolean getCluster(
+    @GetMapping(value = "/increasePartitions")
+    public boolean increasePartitions(
             @ApiParam(value = "kafka地址", allowableValues = "10.202.16.136:9092,192.168.0.105:9092,10.200.3.34:9092")
             @RequestParam(name = "bootstrap.servers", defaultValue = "10.202.16.136:9092")
                     String bootstrap_servers,
